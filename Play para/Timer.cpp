fe13e184f0f6db088586ee8eeabc9ad82f1f3862
Thying <152802890+Thying@users.noreply.github.com>
2023-12-15 19:22:33 +0300

    Сделал вывод цифр с удоление

--- Play para/Timer.cpp
+++ Play para/Timer.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void main() {
-	SetConsoleCP(1251); 
+	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
 	int seconds; // количество секунд до закрытия программы
@@ -15,8 +15,14 @@ void main() {
 	cout << "Введите сколько секунд вы хотите играть: ";
 	cin >> seconds;
 
-	// Задержка в выполнении программы
-	this_thread::sleep_for(chrono::seconds(seconds));
+	for (int i = 0; i < seconds; i++) {
+		// Задержка в выполнении программы
+		this_thread::sleep_for(chrono::seconds(1));
+
+		// Очисшение меню консоли
+		system("cls");
+		cout << seconds - i << endl;
+	}
 
 	cout << "Таймер истек. Программа будет закрыта." << endl;
 
